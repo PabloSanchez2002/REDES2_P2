3161e30f3f4d9bd3ab259e364150030bca3b41a7
PabloS <pablosanchez2002@gmail.com>
2023-03-26 15:58:27 +0200

    colas colas

--- scripts/cliente.py
+++ scripts/cliente.py
@@ -1,10 +1,84 @@
+import os
+import sys
+import uuid
 import pika
+import time
+
+ERROR = 0
+OK = 1
+REGISTERED = 2
+
+def cliente(s,channel):
+    print("Estas registrado como el cliente: "+s)
+    print("Que quieres hacer: ")
+    print("1: Crear Pedido")
+    print("2: Ver Pedidos")
+    print("3: Cancelar Pedidos")
+    try:
+        opcion = int(input())
+        if opcion < 1 or opcion > 3:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Opción no válida seleccione 1, 2 o 3")
+            cliente(s,channel)
+    except ValueError:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Opción no válida seleccione 1, 2 o 3")
+        cliente(s,channel)
+    except KeyboardInterrupt:
+        sys.exit(0)
+
     
+    
+def registro(channel):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Introduce tu identificador de cliente: ", end ="")
+    s = input()
+    channel.basic_publish(exchange='', routing_key='rpc_queue',properties=pika.BasicProperties( reply_to=callback_queue, correlation_id=corr_id), body=s)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    cliente(s,channel)
 
-channel.basic_publish(exchange='',routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
+def crear_cola():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    result = channel.queue_declare(queue='login', exclusive=True, durable=False, auto_delete=True)
+    callback_queue = result.method.queue
+
+    channel.basic_consume(
+        queue=callback_queue, on_message_callback=login_response_callback, auto_ack=True)
+
+    corr_id = str(uuid.uuid4())
+
+    return channel, connection
+
+
+def login_response_callback(ch, method, props, body):
+    response = body
+    if(response == ERROR):
+        print("Error al registrar")
+    
+    elif(response == OK):
+        print("Cliente registrado")
+    
+    elif(response == REGISTERED):
+        print("Cliente registrado")
+    
+    
+def main():
+    channel, connection = crear_cola()
+    registro(channel)
+
+
+
+    connection.close()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- scripts/controlador.py
+++ scripts/controlador.py
@@ -3,6 +3,10 @@ import psycopg2
 import sys
 import os
 
+ERROR = 0
+OK = 1
+REGISTERED = 2
+
 def create_database():
     con = psycopg2.connect(
         database="postgres",
@@ -34,15 +38,21 @@ def create_database():
     )'''
     
     cursor.execute(sql)
-    print("Table created successfully........")
+    print("Table PEDIDOS created successfully........")
+
+    # Creating table as per requirement
+    sql = '''CREATE TABLE CLIENTES(
+    NOMBRE CHAR(30) NOT NULL
+    )'''
+
+    cursor.execute(sql)
+    print("Table CLIENTES created successfully........")
+
     con.commit()
     # Closing the connection
     con.close()
 
-    # Closing the connection
-    con.close()
-
-def new_client():
+def login_client(nombre):
     con = psycopg2.connect(
         database="P2Redes",
         user="",
@@ -51,8 +61,33 @@ def new_client():
         port='5432'
     )
     cursor_obj = con.cursor()
-    cursor_obj.execute("SELECT * FROM bike_details")
+    cursor_obj.execute(
+        "SELECT COUNT(*) as count FROM CLIENTES WHERE NOMBRE = \"" + nombre + "\"")
+    con.commit()
     result = cursor_obj.fetchall()
+
+    if (len(result) > 1):
+
+        # Closing the connection
+        con.close()
+        return ERROR
+        #Mandamos mensaje de error
+    
+    elif(len(result) == 0):
+        cursor_obj.execute("INSERT INTO CLIENTES VALUES (\"" + nombre + "\")")
+        con.commit()
+        # Closing the connection
+        con.close()
+        return REGISTERED
+
+    elif (result[0][0] == 1):
+
+        # Closing the connection
+        con.close()
+        return OK
+    else:
+        return ERROR
+    
     
 
 def main():
@@ -60,18 +95,27 @@ def main():
         pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='login', exclusive=True,
+                          durable=False, auto_delete=True)
 
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+    def rpc_login_client(ch, method, props, body):
+        response = login_client(body)  # Perform the desired operation
+        ch.basic_publish(exchange='',
+                        routing_key=props.reply_to,
+                        properties=pika.BasicProperties(
+                            correlation_id=props.correlation_id),
+                        body=str(response))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_qos(prefetch_count=1)
 
     channel.basic_consume(
-        queue='hello', on_message_callback=callback, auto_ack=True)
+        queue='login', on_message_callback=rpc_login_client)
 
     create_database()
+
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
-
 
 if __name__ == '__main__':
     try:
